9cc7c7244b6f802b8b2c172aa8bee3af2fc71572
Komisarovs <vadim.komisarovs@gmail.com>
2022-04-21 19:17:37 +0300

    Addition

---
+++ Assets/Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script : MonoBehaviour 
+{
+	//Entering the game
+	public void OnMouseDown()
+	{
+		SceneManager.LoadScene (1);
+	}
+	//Exit the game
+	public void ExitClick()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
